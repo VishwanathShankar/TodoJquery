9a0b99fe0f31c7f8a6ee788c3a0e9d2126ec60ac
Home <home@home-Presario-CQ43-Notebook-PC.(none)>
2015-06-10 00:36:01 +0530

    Adding php files

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="toDo.js"></script>
+  <link rel="stylesheet" type="text/css" href="toDo.css">
+</head>
+<body>
+	<div class="container">
+		<div id="header" class="row"> This is my header</div>
---
+++ leftNav.php
@@ -0,0 +1,11 @@
+<div class="row"> 
+	<div class="col-md-3">
+		<ul>
+			<li> 
+				<a href="toDo.php">ToDo</a>
+			 </li>
+			 <li> 
+			 	<a href="toDoNew.php">ToDoNew</a>
+			 </li>
+		</ul>
+	</div>

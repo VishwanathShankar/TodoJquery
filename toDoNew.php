5bb153d10097b982b80b1c4294d239e631467982
Home <home@home-Presario-CQ43-Notebook-PC.(none)>
2015-06-28 12:49:01 +0530

    Comments are removed

--- toDoNew.php
+++ toDoNew.php
@@ -6,17 +6,10 @@
 
 <div class="col-md-9">
 	<div class="row"> 
-		<!--<div class="col-md-6">
-
-			<input type="text" class="add-input"></input> 
-		</div>
-		<div class="col-md-2">
-			<button class="btn btn-primary" id="add-btn">Add</button>
-		</div>-->
-
+		
 	</div>
 
-	<!--<div class="manasa-wrapper">-->
+	
 		<div class="image-wrapper">
 			<p>hello image</p>
 			<div class="image-wrap" id="img">
@@ -25,11 +18,7 @@
 			</div>
 		</div>
 
-	<!--</div>-->
-
-	<!--<div class="row">
-		<button class="btn btn-success" id="save-btn">Save</button>
-	</div>-->
+	
 </div>
 
 
